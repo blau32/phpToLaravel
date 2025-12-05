855b29bc89a77a9e5e243cd2e9adeb214074f612
blau32 <blauschwarz32@gmail.com>
2025-12-05 16:44:40 +0900

    learned how to make form

--- 2.お問い合わせフォーム/form/input.php
+++ 2.お問い合わせフォーム/form/input.php
@@ -1,26 +1,3 @@
-<?php
-if (!empty($_GET)) {
-    // echo $_GET['your_name'];
-    // フォームに入力された文字列が出力される
-    echo '<pre>';
-    var_dump($_GET);
-    echo '</pre>';
-    // 上記に加えてデータ型が表示される
-}
-// これでも取得できる；；
-// スーパーグローバル変数
-//　連想配列
-$pageFlag = 0;
-
-if(!empty($_POST['btn_confirm'])){
-    $pageFlag = 1;
-}
-
-if(!empty($_POST['btn_submit'])){
-    $pageFlag = 2;
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -31,37 +8,67 @@ if(!empty($_POST['btn_submit'])){
 </head>
 
 <body>
+    <!-- フォームに入力された文字列が出力されるようにしたい -->
 
+    <?php
+    if (!empty($_POST)) {
+        // echo $_GET['your_name'];
+        echo '<pre>';
+        var_dump($_POST);
+        echo '</pre>';
+    }
+    // 典型的な３つの確認画面：input confirm, end
+
+    //0なら入力画面
+    $pageFlag = 0;
+
+    // $_POSTにbtn_confirmがあれば1ページ目に遷移
+    if (!empty($_POST['btn_confirm'])) {
+        $pageFlag = 1;
+    }
+
+    // $_POSTにbtn_submitがあれば2ページ目に遷移
+    if (!empty($_POST['btn_submit'])) {
+        $pageFlag = 2;
+    }
+    ?>
+
+    <?php if ($pageFlag == 0): ?>
+        <form method="POST" action="input.php">
+            name
+            <input type="text" name="your_name" value="<?php if(!empty($_POST['your_name']))echo $_POST['your_name']; ?>">
+            <!-- checkボタンを入力して始めてPOSTに値が格納されるため、初回は空の状態。そのため、if(!empty())で値があれば出力するようにする -->
+            <br>
+            mail address
+            <input type='email' name="email" value="<?php if(!empty($_POST['email']))echo $_POST['email']; ?>">
+            <br>
+            <input type="submit" name='btn_confirm' value="check">
+        </form>
+    <?php endif; ?>
 
     <?php if ($pageFlag == 1): ?>
         <form method="POST" action="input.php">
-            name
+            name:
             <?php echo $_POST['your_name']; ?>
             <br>
-            mail address
+            mail address:
             <?php echo $_POST['email']; ?>
             <br>
-            <input type="submit" name='btn_submit' value="check">
+            <input type="submit" name="back" value="back">
+            <input type="submit" name='btn_submit' value="send">
             <input type="hidden" name="your_name" value="<?php echo $_POST['your_name']; ?>">
             <input type="hidden" name="email" value="<?php echo $_POST['email']; ?>">
         </form>
     <?php endif; ?>
 
+    <!-- hidden input を使う理由として、ブラウザを切り替えるとその時点でPOSTされた値が消えてしまうため、hidden input を使って値を保持する
+    -->
+
     <?php if ($pageFlag == 2): ?>
-        sent
+        successfully sent
     <?php endif; ?>
 
-    <?php if ($pageFlag == 0): ?>
-        <form method="GET" action="input.php">
-            name
-            <input type="text" name="your_name">
-            <br>
-            mail address
-            <input type='email' name="email">
-            <br>
-            <input type="submit" name='btn_confirm' value="check">
-        </form>
-    <?php endif; ?>
+
 
 
 </body>
